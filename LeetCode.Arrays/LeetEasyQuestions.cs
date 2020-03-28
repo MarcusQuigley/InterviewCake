4de80e56f4180037919eae43d75bb22ce2097c74
marcus quigley <marcusquigley@gmail.com>
2020-03-28 13:56:41 -0400

    leet 26 remove duplicated. Not working

---
+++ LeetCode.Arrays/LeetEasyQuestions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LeetCode.Arrays
+{
+    public class LeetEasyQuestions
+    {
+        // 26. https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+        public int RemoveDuplicates(int[] numbers)
+        {
+            if (numbers == null) throw new ArgumentNullException("numbers");
+            if (numbers.Length == 0) return 0;
+
+            var currentUniqueValue = numbers[0];
+            var currentUniqueIndex = 0;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > currentUniqueValue)
+                {
+                    if (i != currentUniqueIndex + 1)
+                    {
+                        currentUniqueIndex += 1;
+                        var temp = numbers[i];
+                        numbers[i] = numbers[currentUniqueIndex] ;
+                        numbers[currentUniqueIndex] = temp;
+                        currentUniqueValue = numbers[currentUniqueIndex];
+                    }
+                }
+            }
+            return numbers.Length - currentUniqueIndex ;
+        }
+    }
+}
---
+++ LeetCode.Tests/LeetArraysEasyTests.cs
@@ -0,0 +1,23 @@
+using LeetCode.Arrays;
+using System;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public class LeetArraysEasyTests
+    {
+       
+        [Theory]
+        //[InlineData(new int[] { 1,2,2}, 2)]
+        //[InlineData(new int[] { 1, 1, 2 }, 2)]
+        [InlineData(new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 }, 5)]
+        //[InlineData(new int[] { },0)]
+        [InlineData(new int[] { 1, 2 }, 2)]
+        public void Test_RemoveDuplicates(int[] numbers, int expected)
+        {
+            var sut = new LeetEasyQuestions();
+            var actual = sut.RemoveDuplicates(numbers);
+            Assert.Equal(expected, actual);
+         } 
+    }
+}
