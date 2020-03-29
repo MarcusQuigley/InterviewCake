a7805ba598acc33c8348226432d0955db0068984
marcus quigley <marcusquigley@gmail.com>
2020-03-29 10:54:46 -0400

    renaming class. Adding InHouseCalendar question

---
+++ InterviewCake.Arrays/ArrayQuestions.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InterviewCake.Arrays
+{
+    public class ArrayQuestions
+    {
+        public int FindNumbers(int[] numbers)
+        {
+            if (numbers == null || numbers.Length == 0)
+                return -1;
+            var result = 0;
+
+            //foreach (var number in numbers){
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var number = numbers[i];
+                var counter = 0;
+                while (number > 0)
+                {
+                    number /= 10;
+                    counter += 1;
+                }
+                if (counter % 2 == 0) result += 1;
+            }
+
+            return result;
+        }
+
+        public Meeting[] InHouseCalendar(Meeting[] times)
+        {
+            if (times == null) throw new ArgumentNullException(nameof(times));
+            if (times.Length < 2) return times;
+
+            Array.Sort(times, new MeetingStartComparer());
+
+            var c = 0;
+            Meeting meet1 = null;
+            List<Meeting> results = new List<Meeting>();
+
+            while (c < times.Length)
+            {
+                meet1 = times[c];
+                for (int i = c + 1; i < times.Length; i++)
+                {
+                    var meet2 = times[i];
+                    if (meet1.EndTime >= meet2.StartTime)
+                        meet1.EndTime = meet2.EndTime;
+                    else
+                    {
+                        results.Add(meet1);
+                        c = i;
+                        break;
+                    }
+                }
+            }
+            results.Add(meet1);
+            return results.ToArray();
+        }
+    }
+}
---
+++ InterviewCake.Tests/ArrayTests.cs
@@ -0,0 +1,38 @@
+using System;
+using Xunit;
+using InterviewCake.Arrays;
+
+namespace InterviewCake.Tests
+{
+    public class ArrayTests
+    {
+        ArrayQuestions sut;
+
+        public ArrayTests()
+        {
+            sut = new ArrayQuestions();
+        }
+
+        [Theory]
+        [InlineData(new int[]{12,345,2,6,7896},2)]
+        [InlineData(new int[]{555,901,482,1771},1)]
+        public void Test_FindNumbers(int[] numbers, int expected)
+        {
+             Assert.Equal(expected, sut.FindNumbers(numbers));
+        }
+
+        [Theory]
+        [InlineData(new int[] {0,3,4,10,9 }, new int[] { 1,5,8,12,10})]
+ 
+        public void Test_InHouseCalendar(int[] starts, int[] ends)
+        {
+            var times = new Meeting[starts.Length];
+            for (int i = 0; i < starts.Length; i++)
+            {
+                times[i] = new Meeting(starts[i], ends[i]);
+            }
+            var actual = sut.InHouseCalendar(times);
+            Assert.Equal(1,1 );
+        }
+    }
+}
