62df4ebea4cbda67ee56a41809b60d327883c503
Marcus Quigley <marcusquigley@gmail.com>
2020-04-09 08:11:03 -0400

    WIP CTCI Remove Dups Linked List

---
+++ LinkedLists.Tests/CTCI/CTCIQuestionsLinkedListTests.cs
@@ -0,0 +1,34 @@
+﻿using LinkedLists.CTCI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LinkedLists.Tests.CTCI
+{
+    public class CTCIQuestionsLinkedListTests
+    {
+        CTCIQuestionsLinkedList<int> sut = null;
+        public CTCIQuestionsLinkedListTests()
+        {
+            sut = new CTCIQuestionsLinkedList<int>();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 1 }, 2)]
+        
+        public void TestRemoveDuplicates(int[] values, int expected)
+        {
+            var head = new LLNode<int>(values[0]);
+            var current = head;
+            for (int i = 1; i < values.Length; i++)
+            {
+                //  while(current.next !=null)
+                current.Next = new LLNode<int>(values[i]);
+                current = current.Next;
+            }
+            sut.RemoveDuplicates(head);
+            Assert.Equal(1,1);
+        }
+    }
+}
---
+++ LinkedLists/CTCI/CTCIQuestionsLinkedList.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedLists.CTCI
+{
+  public class CTCIQuestionsLinkedList<T>
+    {
+        public void RemoveDuplicates(LLNode<T> head)
+        {
+            if (head == null) throw new ArgumentNullException(nameof(head));
+            
+            var pointer2 = head.Next;
+            var prev = head;
+            while (head != null)
+            {
+                while (pointer2 != null)
+                {
+                    if (pointer2.Value.Equals(head.Value))
+                    {
+                        prev.Next = pointer2.Next;
+                    }
+                    prev = pointer2;
+                    pointer2 = pointer2.Next;
+                }
+                head = head.Next;
+                prev = head;
+                pointer2 = pointer2.Next;
+            }
+        }
+    }
+}
