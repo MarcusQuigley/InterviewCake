9d168565c2d88701c4d858168c3a260b8b9e7dfd
Marcus Quigley <marcusquigley@gmail.com>
2020-04-12 12:38:43 -0400

    Leet 543 Diameter of Binary Tree

---
+++ Trees.Tests/LeetCode/LeetEasyTreesTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trees.LeetCode;
+using Xunit;
+
+namespace Trees.Tests.LeetCode
+{
+    public class LeetEasyTreesTests
+    {
+        LeetEasyTrees<int> sut = null;
+        public LeetEasyTreesTests()
+        {
+            sut = new LeetEasyTrees<int>();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, 3)]
+        public void TestMiddleNode(int[] values, int expected)
+        {
+            return;
+            var head = CreatTreeNodes(values);
+            var actual = sut.DiameterOfBinaryTree(head);
+            Assert.Equal(expected, actual);
+        }
+
+        private TreeNode<int> CreatTreeNodes(int[] values)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Trees/LeetCode/LeetEasyTrees.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.LeetCode
+{
+   public class LeetEasyTrees<T>
+    {
+        int max;
+        //543 https://leetcode.com/problems/diameter-of-binary-tree/
+        public int DiameterOfBinaryTree(TreeNode<T> root)
+        {
+            Depth(root);
+            return max;
+        }
+        int Depth(TreeNode<T> node)
+        {
+            if (node == null)
+                return 0;
+            int L = Depth(node.Left);
+            int R = Depth(node.Right);
+            max = Math.Max(max, L + R);
+            return Math.Max(L, R) + 1;
+        }
+    }
+}
