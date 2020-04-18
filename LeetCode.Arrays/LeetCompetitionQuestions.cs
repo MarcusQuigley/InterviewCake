33dc5bdec47cb7d9f8e58d0110571268f96a824e
Marcus Quigley <marcusquigley@gmail.com>
2020-04-18 13:31:10 -0400

    Leet competition String shift

---
+++ LeetCode.Arrays/LeetCompetitionQuestions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Arrays
+{
+    public class LeetCompetitionQuestions
+    {
+
+        //https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/529/week-2/3299/
+        public string StringShift(string s, int[][] shift)
+        {
+            if (s.Length < 2)
+                return s;
+
+            int left_shift = 0;
+            int right_shift = 0;
+            foreach (var items in shift)
+            {
+                if (items[0] == 0)
+                    left_shift += items[1];
+                else
+                    right_shift += items[1];
+            }
+            string result = string.Empty;
+            if (left_shift > right_shift)
+            {
+                left_shift -= right_shift;
+                left_shift %= s.Length;
+                result += s.Substring(left_shift, (s.Length - left_shift));
+                result += s.Substring(0, left_shift);
+            }
+            else
+            {
+                right_shift -= left_shift;
+                right_shift %= s.Length;
+                result += s.Substring(s.Length - right_shift, right_shift);
+                result += s.Substring(0, s.Length- right_shift);
+
+            }
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/LeetArraysCompetitionTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCode.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace LeetCode.Tests
+{
+    public  class LeetArraysCompetitionTests
+    {
+        LeetCompetitionQuestions sut;
+        public LeetArraysCompetitionTests()
+        {
+            sut = new LeetCompetitionQuestions();
+        }
+        //[Theory]
+        //[InlineData("abc", new int[] { { 0, 1 }{ (1, 2) }, "cab")]
+        // public void Test_GroupAnagrams(string value, int[][] mask, string expected)
+        //{
+        //    var actual = sut.StringShift(value, mask);
+        //    Assert.Equal(expected, actual);
+        //}
+
+         
+
+    }
+}
