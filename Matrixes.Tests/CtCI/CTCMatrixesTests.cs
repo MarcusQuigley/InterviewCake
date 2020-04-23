4c2ff9cf324992942c41be3cf520884618f3d50d
Marcus Quigley <marcusquigley@gmail.com>
2020-04-23 19:20:10 -0400

    CtCI Rotate matrix. WIP

---
+++ Matrixes.Tests/CtCI/CTCMatrixesTests.cs
@@ -0,0 +1,25 @@
+﻿using Matrixes.CTCI;
+using Xunit;
+
+namespace Matrixes.Tests.CtCI
+{
+    public  class CTCMatrixesTests
+    {
+         CTCIMatrixes sut = null;
+        public CTCMatrixesTests()
+        {
+            sut = new CTCIMatrixes();
+        }
+        [Fact]
+        public void TestRotateMatrix()
+        {
+            var grid = new int[4][];
+            grid[0] = new int[] { 1, 2, 3, 4 };
+            grid[1] = new int[] { 5, 6, 7, 8 };
+            grid[2] = new int[] { 9, 10, 11, 12 };
+            grid[3] = new int[] { 13, 14, 15, 16 };
+            sut.RotateMatrix(grid);
+            Assert.Equal("", "Need to fix this RotateMatrix");
+        }
+    }
+}
---
+++ Matrixes/CTCI/CTCMatrixes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrixes.CTCI
+{
+    public class CTCIMatrixes
+    {
+
+        public void RotateMatrix(int[][] matrix)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+            var n = matrix.Length;
+            if (n < 2) 
+                return;
+
+            for (int i = 0; i < n/2; i++)
+            {
+                var first = i;
+                var last = n - 1 - i;
+                for (int j = first; j < last; j++)
+                {
+                    var offset = j - first;
+                    var temp = matrix[offset][j];
+                //    matrix[offset][j] = matrix[last - offset][j];
+                //    matrix[end - 1 - offset][j] = matrix[end - 1 - offset][end - 1 - offset];
+                //    matrix[end - 1 - offset][end - 1 - offset] = matrix[offset][end - 1 - offset] ;
+                //    matrix[offset][end - 1 - offset] = temp;
+                 }
+            }
+        }
+    }
+}
