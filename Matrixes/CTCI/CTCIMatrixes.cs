d9c8c1fe4ae1afd670c9c3719a709e4b46628584
Marcus Quigley <marcusquigley@gmail.com>
2020-04-24 11:24:00 -0400

    CTCI 1.8 Zero Matrix

--- Matrixes/CTCI/CTCIMatrixes.cs
+++ Matrixes/CTCI/CTCIMatrixes.cs
@@ -18,16 +18,16 @@ namespace Matrixes.CTCI
             for (int i = 0; i < n/2; i++)
             {
                 var first = i;
-                var last = n - 1 - i;
+                var last = n - 1 - first;
                 for (int j = first; j < last; j++)
                 {
                     var offset = j - first;
-                    var temp = matrix[offset][j];
-                //    matrix[offset][j] = matrix[last - offset][j];
-                //    matrix[end - 1 - offset][j] = matrix[end - 1 - offset][end - 1 - offset];
-                //    matrix[end - 1 - offset][end - 1 - offset] = matrix[offset][end - 1 - offset] ;
-                //    matrix[offset][end - 1 - offset] = temp;
-                 }
+                    var temp = matrix[first][j];
+                    matrix[first][j] = matrix[last-offset][first];
+                    matrix[last - offset][first] = matrix[last][last - offset];
+                    matrix[last][last - offset] = matrix[j][last];
+                    matrix[j][last] = temp;
+                }
             }
         }
     }
