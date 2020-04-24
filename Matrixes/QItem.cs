95842c612f8dbbd80f54a6990996cc25d56ac872
Marcus Quigley <marcusquigley@gmail.com>
2020-04-24 13:29:27 -0400

    Geeks Shortest Path in matrix

---
+++ Matrixes/QItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matrixes
+{
+   public class BaseQItem
+    {
+        public BaseQItem(int row, int col)
+        {
+            Row = row;
+            Col = col;
+        }
+
+        public int Row { get; set; }
+        public int Col { get; set; }
+        
+    }
+
+    public class QItem : BaseQItem
+    {
+        public QItem(int row, int col, int total)
+            :base(row, col)
+        {
+            Total = total;
+        }
+        public int Total { get; set; }
+    }
+}
