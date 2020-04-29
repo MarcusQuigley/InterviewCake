235a2702fdd51a0f265f3bea1ce289ea6b4440dc
Marcus Quigley <marcusquigley@gmail.com>
2020-04-29 16:48:52 -0400

    Leet M 146 LRUCache. BAD Time complexity

--- Design.Tests/LeetCode/LeetMediumDesignTests.cs
+++ Design.Tests/LeetCode/LeetMediumDesignTests.cs
@@ -1,15 +1,45 @@
-﻿using System;
+﻿using Design.LeetCode.Medium;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
 
 namespace Design.Tests.LeetCode
 {
-  public class LeetMediumDesignTests
+    public class LeetMediumDesignTests
     {
         public LeetMediumDesignTests()
         {
+    
+        }
 
+
+    [Fact]
+        public void Test_LRUCache()
+        {
+            LRUCache cache = new LRUCache(2);
+            cache.Put(2, 1);
+            cache.Put(1, 1);
+            cache.Put(2, 3);
+            cache.Put(4, 1);
+            Assert.Equal(-1, cache.Get(1));       // returns 1
+            Assert.Equal(3, cache.Get(2));    // returns -1
+         }
+
+        [Fact]
+        public void Test_LRUCache1()
+        {
+            LRUCache cache = new LRUCache(2 );
+
+            cache.Put(1, 1);
+            cache.Put(2, 2);
+            Assert.Equal(1, cache.Get(1));       
+            cache.Put(3, 3);   
+            Assert.Equal(-1, cache.Get(2));     
+            cache.Put(4, 4);    
+            Assert.Equal(-1, cache.Get(1));      
+            Assert.Equal(3, cache.Get(3));      
+            Assert.Equal(4, cache.Get(4));        
         }
     }
 }
