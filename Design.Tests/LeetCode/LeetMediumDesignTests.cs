d33d76dce5489651e9d4c5626be571073a884291
Marcus Quigley <marcusquigley@gmail.com>
2020-04-29 09:38:45 -0400

    Design init commit

---
+++ Design.Tests/LeetCode/LeetMediumDesignTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Design.Tests.LeetCode
+{
+  public class LeetMediumDesignTests
+    {
+        public LeetMediumDesignTests()
+        {
+
+        }
+    }
+}
