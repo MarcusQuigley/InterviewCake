8ae7e84f2ce3dcbcd89eae925aef643b6a7a39aa
Marcus Quigley <marcusquigley@gmail.com>
2020-04-30 00:38:51 -0400

    Leet M 1143 Longest Common Subsequence

--- DynamicProgramming.Tests/LeetCode/LeetMediumDPTests.cs
+++ DynamicProgramming.Tests/LeetCode/LeetMediumDPTests.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using DynamicProgramming.LeetCode;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace DynamicProgramming.Tests.LeetCode
 {
     public class LeetMediumDPTests
     {
+        LeetMediumDP sut = null;
+        public LeetMediumDPTests()
+        {
+            sut = new LeetMediumDP();
+        }
+
+        [Theory]
+        [InlineData("abcde", "ace",3)]
+        [InlineData("ace", "ace", 3)]
+        [InlineData("abc", "def", 0)]
+        public void Test_LongestCommonSubsequence(string text1, string text2, int expected)
+        {
+            var actual = sut.LongestCommonSubsequence(text1, text2);
+            Assert.Equal(expected, actual);
+        }
     }
 }
--- DynamicProgramming/LeetCode/LeetMediumDP.cs
+++ DynamicProgramming/LeetCode/LeetMediumDP.cs
@@ -6,5 +6,32 @@ namespace DynamicProgramming.LeetCode
 {
     public class LeetMediumDP
     {
+
+        //1143 https://leetcode.com/problems/longest-common-subsequence/
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            if (text1 == null || text2 == null)
+                throw new ArgumentNullException("one param at least is null");
+
+            var rows = text1.Length + 1;
+            var cols = text2.Length + 1;
+        
+            int[][] matrix = new int[rows][];
+            for (int i = 0; i < rows; i++)
+                matrix[i] = new int[cols];
+ 
+
+            for (int r = 1; r < rows; r++)
+            {
+                for (int c = 1; c < cols; c++)
+                {
+                    if (text1[r-1] == text2[c-1])
+                        matrix[r][c] = matrix[r-1 ][c - 1] + 1;
+                    else
+                        matrix[r][c] = Math.Max(matrix[r- 1][c], matrix[r][c-1]);
+                }
+            }
+            return matrix[rows-1][cols-1];
+        }
     }
 }
