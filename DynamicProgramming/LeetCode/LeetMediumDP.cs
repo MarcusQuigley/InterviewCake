3d3b10437c6d05355cc8573900d7af00b91e2c61
Marcus Quigley <marcusquigley@gmail.com>
2020-04-30 11:49:29 -0400

    Leet 221 Maximal Square

--- DynamicProgramming/LeetCode/LeetMediumDP.cs
+++ DynamicProgramming/LeetCode/LeetMediumDP.cs
@@ -33,5 +33,36 @@ namespace DynamicProgramming.LeetCode
             }
             return matrix[rows-1][cols-1];
         }
+
+        //221 https://leetcode.com/problems/maximal-square/
+        public int MaximalSquare(char[][] matrix)
+        {
+            if (matrix == null)
+                throw new ArgumentNullException(nameof(matrix));
+            if (matrix.Length == 0 || matrix[0].Length==0)
+                return 0;
+            var rows = matrix.Length+1;
+            var cols = matrix[0].Length+1;
+
+            var resultMatrix = new int[rows][];
+            for (int i = 0; i < rows; i++)
+                resultMatrix[i] = new int[cols];
+            var max = int.MinValue;
+
+            for (int r = 1; r < rows; r++)
+            {
+                for (int c = 1; c < cols; c++)
+                {
+                    if (matrix[r - 1][c - 1] == '1')
+                    {
+                        var mintemp = Math.Min(resultMatrix[r][c - 1] , resultMatrix[r - 1][c - 1]);
+                        var min = Math.Min(mintemp, resultMatrix[r - 1][c])+1;
+                        resultMatrix[r][c] = min;
+                        max = Math.Max(max, min);
+                    }
+                }
+            }
+            return max * max;
+        }
     }
 }
