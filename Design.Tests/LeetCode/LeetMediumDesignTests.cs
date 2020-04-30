896047751d3e0ecf7e0dca4f9031e7c36810fa44
Marcus Quigley <marcusquigley@gmail.com>
2020-04-30 14:16:52 -0400

    Leet FirstUnique data structure. Times out on huge ops

--- Design.Tests/LeetCode/LeetMediumDesignTests.cs
+++ Design.Tests/LeetCode/LeetMediumDesignTests.cs
@@ -10,11 +10,10 @@ namespace Design.Tests.LeetCode
     {
         public LeetMediumDesignTests()
         {
-    
+
         }
-
-
-    [Fact]
+ 
+        [Fact]
         public void Test_LRUCache()
         {
             LRUCache cache = new LRUCache(2);
@@ -24,22 +23,57 @@ namespace Design.Tests.LeetCode
             cache.Put(4, 1);
             Assert.Equal(-1, cache.Get(1));       // returns 1
             Assert.Equal(3, cache.Get(2));    // returns -1
-         }
+        }
 
         [Fact]
         public void Test_LRUCache1()
         {
-            LRUCache cache = new LRUCache(2 );
+            LRUCache cache = new LRUCache(2);
 
             cache.Put(1, 1);
             cache.Put(2, 2);
-            Assert.Equal(1, cache.Get(1));       
-            cache.Put(3, 3);   
-            Assert.Equal(-1, cache.Get(2));     
-            cache.Put(4, 4);    
-            Assert.Equal(-1, cache.Get(1));      
-            Assert.Equal(3, cache.Get(3));      
-            Assert.Equal(4, cache.Get(4));        
+            Assert.Equal(1, cache.Get(1));
+            cache.Put(3, 3);
+            Assert.Equal(-1, cache.Get(2));
+            cache.Put(4, 4);
+            Assert.Equal(-1, cache.Get(1));
+            Assert.Equal(3, cache.Get(3));
+            Assert.Equal(4, cache.Get(4));
+        }
+
+        [Fact]
+        public void Test_ShowFirstUnique()
+        {
+            FirstUnique sut = new FirstUnique(new int[] { 2, 3, 5 });
+            Assert.Equal(2, sut.ShowFirstUnique());
+            sut.Add(5);
+            Assert.Equal(2, sut.ShowFirstUnique());
+            sut.Add(2);
+            Assert.Equal(3, sut.ShowFirstUnique());
+            sut.Add(3);
+            Assert.Equal(-1, sut.ShowFirstUnique());
+        }
+
+        [Fact]
+        public void Test_ShowFirstUnique2()
+        {
+            FirstUnique sut = new FirstUnique(new int[] { 7, 7, 7, 7, 7, 7 });
+            Assert.Equal(-1, sut.ShowFirstUnique());
+            sut.Add(7);
+            sut.Add(3);
+            sut.Add(3);
+            sut.Add(7);
+            sut.Add(17);
+            Assert.Equal(17, sut.ShowFirstUnique());
+        }
+
+        [Fact]
+        public void Test_ShowFirstUnique3()
+        {
+            FirstUnique sut = new FirstUnique(new int[] {809});
+            Assert.Equal(809, sut.ShowFirstUnique());
+            sut.Add(809);
+            Assert.Equal(-1, sut.ShowFirstUnique());
         }
     }
 }
---
+++ Design/LeetCode/Medium/FirstUnique.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Design.LeetCode.Medium
+{
+    //https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/531/week-4/3313/
+    public class FirstUnique
+    {
+        Dictionary<int, LinkedListNode<int>> map;
+        LinkedList<int> llist;
+
+        public FirstUnique(int[] nums)
+        {
+            if (nums == null)
+                throw new ArgumentNullException(nameof(nums));
+
+            map = new Dictionary<int, LinkedListNode<int>>();
+            llist = new LinkedList<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Add(nums[i]);
+            }
+        }
+
+        public void Add(int value)
+        {
+            if (map.ContainsKey(value))
+            {
+                map[value] = null;
+                llist.Remove(value);
+            }
+            else
+            {
+                LinkedListNode<int> node = new LinkedListNode<int>(value);
+                map.Add(value, node);
+                llist.AddLast(node);
+            }
+        }
+
+        public int ShowFirstUnique()
+        {
+            if (llist.Count == 0)
+                return -1;
+            return llist.First.Value;
+        }
+    }
+}
