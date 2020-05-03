147fcfb45180005c0efea3c55cfaa8c1ccbe289b
Marcus Quigley <marcusquigley@gmail.com>
2020-05-03 08:13:38 -0400

    Leet Reverse String

---
+++ Recursion/LeetCode/RecursionOne.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Recursion.LeetCode
+{
+    public class RecursionOne
+    {
+        //https://leetcode.com/explore/featured/card/recursion-i/250/principle-of-recursion/1440/
+
+        public void ReverseString(char[] s)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
+
+            RecursiveReverseString(s, 0, s.Length-1);
+        }
+
+        void RecursiveReverseString(char[] s, int left, int right)
+        {
+            if (left >= right) return;
+
+            var temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            RecursiveReverseString(s, left + 1, right - 1);
+        }
+
+        public char[] ReverseStringIter(char[] s)
+        {
+            if (s == null) throw new ArgumentNullException(nameof(s));
+            if (s.Length >1 )
+                {
+                var left = 0;
+                var right = s.Length - 1;
+                while (left < right)
+                {
+                    var temp = s[right];
+                    s[right] = s[left];
+                    s[left] = temp;
+                    left++;
+                    right--;
+                }
+            }
+            return s;
+        }
+    }
+}
---
+++ Tests/Recursion.Tests/LeetCode/RecursionOneTests.cs
@@ -0,0 +1,30 @@
+﻿using Recursion.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Recursion.Tests.LeetCode
+{
+  public  class RecursionOneTests
+    {
+        RecursionOne sut;
+
+        public RecursionOneTests()
+        {
+            sut = new RecursionOne();
+        }
+
+        [Theory]
+        [InlineData("abc","cba")]
+        [InlineData("ab", "ba")]
+        [InlineData("hello", "olleh")]
+        public void Test_ReverseString(string s1, string expected)
+        {
+            var s1chararray = s1.ToCharArray();
+             sut.ReverseString(s1chararray);
+            var actual = new string(s1chararray);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
