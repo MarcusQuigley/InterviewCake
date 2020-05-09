f3ca8103e6b7532207fb3e142e2e61b0ed73ea63
Marcus Quigley <marcuquigley@gmail.com>
2020-05-09 19:18:13 -0400

    Leet M 142. Linked List Cycle II

---
+++ LinkedLists/LeetCode/LeetMediumLinkedList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedLists.LeetCode
+{
+   public class LeetMediumLinkedList
+    {
+
+
+        //141 https://leetcode.com/problems/linked-list-cycle/
+        public ListNode DetectCycleOld(ListNode head)
+        {
+            if (head != null)
+            {
+                var p1 = head;
+                var p2 = head;
+                while (p1 !=null && p2 != null)
+                {
+                    if (p1 == p2) {
+                        p1 = head;
+                        while (true)
+                        {
+                            if (p1 == p2)
+                                return p1;
+                            p1 = p1.next;
+                            p2 = p2.next;
+                        }
+                    }
+                    else
+                    {
+                        if (p2.next == null)
+                            return null;
+                        p1 = p1.next;
+                        p2 = p2.next.next;
+                    }
+                }
+            }
+            return null;
+        }
+
+        public ListNode DetectCycle(ListNode head)
+        {
+            if (head == null) return null;
+            var intersect = GetIntersect(head);
+            if (intersect == null) return null; //no cycle;
+
+
+            while (head != intersect)
+            {
+                head = head.next;
+                intersect = intersect.next;
+            }
+            return head;
+        }
+
+        ListNode GetIntersect(ListNode node)
+        {
+            var p1 = node;
+            var p2 = node;
+            while(p1!=null && p2.next != null)
+            {
+                p1 = p1.next;
+                p2 = p2.next.next;
+                if (p1 == p2)
+                    return p1;
+            }
+            return null;
+        }
+    }
+}
