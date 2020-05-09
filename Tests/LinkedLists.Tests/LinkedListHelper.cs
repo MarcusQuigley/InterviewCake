0c1cd1f202aeb3bce9ce3e8872b19ab8b038ef6a
Marcus Quigley <marcuquigley@gmail.com>
2020-05-09 14:03:48 -0400

    cleanup

---
+++ Tests/LinkedLists.Tests/LinkedListHelper.cs
@@ -0,0 +1,26 @@
+﻿using LinkedLists.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedLists.Tests
+{
+   public static class LinkedListHelper
+    {
+        #region Helpers
+
+        public static ListNode CreateLinkedList(int[] values)
+        {
+            var head = new ListNode(values[0]);
+            var current = head;
+            for (int i = 1; i < values.Length; i++)
+            {
+                //  while(current.next !=null)
+                current.next = new ListNode(values[i]);
+                current = current.next;
+            }
+            return head;
+        }
+        #endregion
+    }
+}
