c9b8db114022ca59aeff18d2ddbff6313a338ab8
Marcus Quigley <marcuquigley@gmail.com>
2020-05-13 12:30:53 -0400

    Leet E 1089 Duplicate Zeros

--- Tests/LeetCode.Tests/LeetCode/LeetArraysEasyTests.cs
+++ Tests/LeetCode.Tests/LeetCode/LeetArraysEasyTests.cs
@@ -307,7 +307,20 @@ namespace Arrays.Tests.LeetCode
             var actual = sut.SortedSquares(nums);
             Assert.Equal(expected, actual);
         }
+        [Theory]
+        [InlineData(new int[] { 1, 0, 2, 3, 0, 4, 5, 0 }, new int[] { 1, 0, 0, 2, 3, 0, 0, 4 })]
+        [InlineData(new int[] { 1, 2, 3 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { 1, 2, 0, 3, 0, 7, 9, 8 }, new int[] { 1, 2, 0, 0, 3, 0, 0, 7 })]
+        [InlineData(new int[] { 0 }, new int[] { 0 })]
+        [InlineData(new int[] { 1, 1, 0, 0 }, new int[] { 1, 1, 0, 0 })]
+        [InlineData(new int[] { 8, 4, 5, 0, 0, 0, 0, 7 }, new int[] { 8, 4, 5, 0, 0, 0, 0, 0 })]
+        [InlineData(new int[] { 9, 9, 9, 4, 8, 0, 0, 3, 7, 2, 0, 0, 0, 0, 9, 1, 0, 0, 1, 1, 0, 5, 6, 3, 1, 6, 0, 0, 2, 3, 4, 7, 0, 3, 9, 3, 6, 5, 8, 9, 1, 1, 3, 2, 0, 0, 7, 3, 3, 0, 5, 7, 0, 8, 1, 9, 6, 3, 0, 8, 8, 8, 8, 0, 0, 5, 0, 0, 0, 3, 7, 7, 7, 7, 5, 1, 0, 0, 8, 0, 0 },
+            new int[] { 9, 9, 9, 4, 8, 0, 0, 0, 0, 3, 7, 2, 0, 0, 0, 0, 0, 0, 0, 0, 9, 1, 0, 0, 0, 0, 1, 1, 0, 0, 5, 6, 3, 1, 6, 0, 0, 0, 0, 2, 3, 4, 7, 0, 0, 3, 9, 3, 6, 5, 8, 9, 1, 1, 3, 2, 0, 0, 0, 0, 7, 3, 3, 0, 0, 5, 7, 0, 0, 8, 1, 9, 6, 3, 0, 0, 8, 8, 8, 8, 0 })]
 
-
+        public void Test_DuplicateZeros(int[] nums, int[] expected)
+        {
+            sut.DuplicateZeros(nums);
+            Assert.Equal(expected, nums);
+        }
     }
 }
