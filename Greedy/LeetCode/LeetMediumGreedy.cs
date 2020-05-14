e900deba43791937c7a0a6e61245527b484edbc2
Marcus Quigley <marcuquigley@gmail.com>
2020-05-14 10:00:10 -0400

    init commit

---
+++ Greedy/LeetCode/LeetMediumGreedy.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Greedy.LeetCode
+{
+  public class LeetMediumGreedy
+    {
+    }
+}
---
+++ Tests/Greedy.Tests/LeetCode/LeetMediumGreedyTests.cs
@@ -0,0 +1,16 @@
+﻿using Greedy.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Greedy.Tests.LeetCode
+{
+    public class LeetMediumGreedyTests
+    {
+        LeetMediumGreedy sut = null;
+        public LeetMediumGreedyTests()
+        {
+            sut = new LeetMediumGreedy();
+        }
+    }
+}
