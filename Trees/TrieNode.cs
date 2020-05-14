61d9141eb29b81fe1619b32a2e1bf7f35719ca22
Marcus Quigley <marcuquigley@gmail.com>
2020-05-14 17:07:55 -0400

    Leet 208 M Implement Trie

---
+++ Trees/TrieNode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    class TrieNode
+    {
+        TrieNode[] links;
+        int final = 26;
+        bool isEnded;
+
+        public TrieNode()
+        {
+            links = new TrieNode[final];
+        }
+        public bool IsEnded { get; set; }
+
+        public bool Contains(char ch)
+        {
+            return links[ch - 'a'] != null;
+        }
+
+        public TrieNode Get(char ch)
+        {
+            return links[ch - 'a'];
+        }
+
+        public void Put(char ch, TrieNode node)
+        {
+            links[ch - 'a'] = node;
+        }
+    }
+}
