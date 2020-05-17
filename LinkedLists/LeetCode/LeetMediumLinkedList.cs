c3faa45247796cd8f34bb0459d9a882be0d7b578
Marcus Quigley <marcuquigley@gmail.com>
2020-05-17 19:13:26 -0400

    Leet M 328 Odd Even List

--- LinkedLists/LeetCode/LeetMediumLinkedList.cs
+++ LinkedLists/LeetCode/LeetMediumLinkedList.cs
@@ -87,5 +87,20 @@ namespace LinkedLists.LeetCode
             return head;
 
         }
+        //328 https://leetcode.com/problems/odd-even-linked-list/
+         public ListNode OddEvenList(ListNode head)
+        {
+            if (head == null) return null;
+            ListNode odd = head, even = head.next, evenHead = even;
+            while (even != null && even.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenHead;
+            return head;
+        }
     }
 }
