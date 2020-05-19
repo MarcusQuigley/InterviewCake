503198e0308eae4ef6e5ed452ecab6899dfd501f
Marcus Quigley <marcuquigley@gmail.com>
2020-05-19 16:55:57 -0400

    get stocks profit InterviewCake

---
+++ Tests/Greedy.Tests/InterviewCake/InterviewCakeGreedyTests.cs
@@ -0,0 +1,24 @@
+﻿using Greedy.InterviewCake;
+using System;
+using Xunit;
+
+namespace Greedy.Tests.InterviewCake
+{
+   public class InterviewCakeGreedyTests
+    {
+      readonly  InterviewCakeGreedy sut = null;
+        public InterviewCakeGreedyTests()
+        {
+            sut = new InterviewCakeGreedy();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 10, 7, 5, 8, 11, 9 }, 6)]
+        [InlineData(new int[] { 13, 7, 5, 8, 4, 9 }, 5)]
+        [InlineData(new int[] { 10, 7, 5, 3,2 }, -1)]
+        public void Test_GetMaxProfit(int[] stockPrices, int expected) {
+            var actual = sut.GetMaxProfit(stockPrices);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
