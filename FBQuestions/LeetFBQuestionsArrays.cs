767ced3c584dfd2175c515e42a5d130b24ce1923
Marcus Quigley <marcuquigley@gmail.com>
2020-05-24 11:56:33 -0400

    Leet M FBQ Interval List intersections

--- FBQuestions/LeetFBQuestionsArrays.cs
+++ FBQuestions/LeetFBQuestionsArrays.cs
@@ -51,5 +51,58 @@ namespace FBQuestions
             }
             return true;
         }
+
+        //986 https://leetcode.com/problems/interval-list-intersections/
+        //Medium
+        public int[][] IntervalIntersection(int[][] A, int[][] B)
+        {
+            if (A == null || B == null)
+                throw new ArgumentNullException("One or both params null");
+            var results = new List<int[]>();
+            var aIndex = 0;
+            var bIndex = 0;
+             while(aIndex<A.Length && bIndex < B.Length)
+            {
+                var maxStart = Math.Max(A[aIndex][0], B[bIndex][0]);
+                var minEnd = Math.Min(A[aIndex][1], B[bIndex][1]);
+                if(maxStart <= minEnd)
+                    results.Add(new int[] { maxStart, minEnd });
+
+                if (minEnd == A[aIndex][1])
+                    aIndex++;
+                else
+                    bIndex++;
+            }
+            return results.ToArray();
+        }
+
+        CompareResult ComparePoints(int[] pointA, int[] pointB)
+        {
+            var result = new CompareResult();
+            if (pointA[0] == pointB[0])
+            {
+                if (pointA[1] == pointB[1]) //both equal
+                    return new CompareResult() { Intersects = true, Intersection = pointA, IncrementAIndex = true, IncrementBIndex = true };
+                if(pointA[1]<pointB[1])
+                    return new CompareResult() { Intersects = true, Intersection = pointA, IncrementAIndex = true, IncrementBIndex = false };
+                else
+                    return new CompareResult() { Intersects = true, Intersection = pointB, IncrementAIndex = false, IncrementBIndex = true };
+            }
+            else
+            {
+                var earlier = pointA[0] < pointB[0] ? pointA : pointB;
+                var later = pointA[0] < pointB[0] ? pointB : pointA;
+                //if(earlier[1]<later[0]) //earlier ends before later starts no intersection
+            }
+            return result;
+        }
+
+        private struct CompareResult
+        {
+            public bool Intersects { get; set; }
+            public bool IncrementAIndex { get; set; }
+            public bool IncrementBIndex { get; set; }
+            public int[] Intersection { get; set; }
+        }
     }
 }
--- Tests/FBQuestions.Tests/LeetFBQuestionsArraysTests.cs
+++ Tests/FBQuestions.Tests/LeetFBQuestionsArraysTests.cs
@@ -23,5 +23,34 @@ namespace FBQuestions.Tests
             var actual = sut.IsAlienSorted(words, order);
             Assert.Equal(expected, actual);
         }
+
+
+        [Fact]
+        //[InlineData(new string[] { "hello", "leetcode" }, "hlabcdefgijkmnopqrstuvwxyz", true)]
+        public void Test_IntervalIntersection()//int[][] A, int[][] B, int[][] expected)
+        {
+            var A = new int[4][];
+            A[0] = new int[] { 0, 2 };
+            A[1] = new int[] { 5,10 };
+            A[2] = new int[] { 13,23 };
+            A[3] = new int[] { 24,25 };
+
+            var B = new int[4][];
+            B[0] = new int[] { 1,5 };
+            B[1] = new int[] { 8, 12 };
+            B[2] = new int[] { 15, 24 };
+            B[3] = new int[] { 25,26 };
+
+            var expected = new int[6][];
+            expected[0] = new int[] { 1, 2 };
+            expected[1] = new int[] { 5,5};
+            expected[2] = new int[] { 8,10 };
+            expected[3] = new int[] { 15,23 };
+            expected[4] = new int[] { 24,24 };
+            expected[5] = new int[] { 25, 25 };
+
+            var actual = sut.IntervalIntersection(A, B);
+            Assert.Equal(expected, actual);
+        }
     }
 }
