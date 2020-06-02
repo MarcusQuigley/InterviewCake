752d39000326528d74ff8ac816c97d943e2261e2
Marcus Quigley <marcuquigley@gmail.com>
2020-06-02 16:50:12 -0400

    Leet Study Linked List GetIntersection, ReverseList, RemoveNodes, Palindrome, OddEven

--- LeetStudy/LeetStudyLinkedLists.cs
+++ LeetStudy/LeetStudyLinkedLists.cs
@@ -14,7 +14,7 @@ namespace LeetStudy
                 throw new ArgumentNullException(nameof(head));
             var runner = head.next;
 
-            while(head!=null && runner!=null && runner.next != null)
+            while (head != null && runner != null && runner.next != null)
             {
                 if (head == runner)
                     return true;
@@ -24,6 +24,143 @@ namespace LeetStudy
             return false;
         }
 
-       
+        //https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1215/
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+                throw new ArgumentNullException("params null");
+            var tempA = headA;
+            var tempB = headB;
+            var countA = NodeCount(ref tempA);
+            var countB = NodeCount(ref tempB);
+            if (tempA != tempB)
+                return null;
+            if (countA > countB)
+            {
+                while (countA > countB)
+                {
+                    headA = headA.next;
+                    countA -= 1;
+                }
+            }
+            else
+            {
+                while (countB > countA)
+                {
+                    headB = headB.next;
+                    countB -= 1;
+                }
+            }
+            while (headA != headB)
+            {
+                headA = headA.next;
+                headB = headB.next;
+            }
+            return headA;
+
+        }
+
+        int NodeCount(ref ListNode node)
+        {
+            int count = 0;
+            while (node != null)
+            {
+                count += 1;
+                node = node.next;
+            }
+            return count;
+        }
+        //https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1205/
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+                throw new ArgumentNullException(nameof(head));
+
+            var current = head;
+
+            while (current.next != null)
+            {
+                var p = current.next;
+                current.next = p.next;
+                p.next = head;
+                current = p;
+            }
+            return head;
+        }
+
+        //https://leetcode.com/explore/learn/card/linked-list/219/classic-problems/1207/
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            if (head != null)
+            {
+                while (head.val == val)
+                {
+                    if (head.next == null)
+                        return null;
+                    head = head.next;
+                }
+                ListNode prev = null;
+                var current = head;
+                while (current != null)
+                {
+                    if (current.val == val)
+                        prev.next = current.next;
+                    else
+                        prev = current;
+                    current = current.next;
+                }
+            }
+            return head;
+        }
+
+        public ListNode OddEvenList(ListNode head)
+        {
+            if (head == null)
+                throw new ArgumentNullException(nameof(head));
+            var odd = head;
+            var even = head.next;
+            var evenhead = even;
+            while (odd.next != null)
+            {
+                odd.next = even.next;
+                odd = odd.next;
+                even.next = odd.next;
+                even = even.next;
+            }
+            odd.next = evenhead;
+            return head;
+        }
+
+        public bool IsPalindrome(ListNode head)
+        {
+            if (head == null) return true;
+            if (head.next == null) return true;
+
+            var fast = head.next;
+            var slow = head;
+            while (fast != null && fast.next != null)
+            {
+                fast = fast.next.next;
+                slow = slow.next;
+            }
+            slow = slow.next;
+            //reverse rest of slow;
+            var currentSlow = slow;
+            while (slow.next != null)
+            {
+                var p = slow.next;
+                slow.next = p.next;
+                p.next = currentSlow;
+                currentSlow = p;
+            }
+            while (currentSlow != null)
+            {
+                if (currentSlow.val != head.val)
+                    return false;
+                currentSlow = currentSlow.next;
+                head = head.next;
+            }
+            return true;
+        }
     }
 }
--- Tests/LeetStudy.Tests/LeetStudyLinkedListTests.cs
+++ Tests/LeetStudy.Tests/LeetStudyLinkedListTests.cs
@@ -28,14 +28,53 @@ namespace LeetStudy.Tests
 
         [Theory]
         [InlineData(new int[] { 4, 1, 8, 4, 5 }, new int[] { 5, 0, 1 }, 8)]
-        //public void Test_GetIntersectionNode(int[] values, int[] values2, int expected)
-        //{
-        //    ListNode nodeA = LinkedListHelper.CreateLinkedList(values);
-        //    ListNode nodeB = LinkedListHelper.CreateLinkedList(values2);
-        //    nodeB.next.next.next = nodeA.next.next;
+        public void Test_GetIntersectionNode(int[] values, int[] values2, int expected)
+        {
+            ListNode nodeA = LinkedListHelper.CreateLinkedList(values);
+            ListNode nodeB = LinkedListHelper.CreateLinkedList(values2);
+            nodeB.next.next.next = nodeA.next.next;
 
-        //    var actual = sut.GetIntersectionNode(nodeA, nodeB);
-        //    Assert.Equal(expected, actual.val);
-        //}
+            var actual = sut.GetIntersectionNode(nodeA, nodeB);
+            Assert.Equal(expected, actual.val);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1,2,6,3,4,5,6 }, 6, new int[] { 1, 2, 3, 4, 5 })]
+        [InlineData(new int[] { 1 }, 1, new int[] {  })]
+        [InlineData(new int[] { 1 ,2,2,1}, 2, new int[] {1,1 })]
+        public void Test_RemoveElements(int[] values, int val, int[] expected)
+        {
+            ListNode nodeA = LinkedListHelper.CreateLinkedList(values);
+             var actual = sut.RemoveElements(nodeA, val);
+            Assert.Equal(expected, LinkedListHelper.ListFromListNode(actual));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 4, 5 }, new int[] { 1, 3, 5, 2, 4 })]
+        //[InlineData(new int[] {  2,1,3,5,6,4,7}, new int[] { 2, 3,6,7,1, 5,4 })]
+        //[InlineData(new int[] { 1 },  new int[] {1 })]
+        //[InlineData(new int[] { 1, 2 }, new int[] { 1,2 })]
+        public void Test_OddEvenList(int[] values, int[] expected)
+        {
+            ListNode node = LinkedListHelper.CreateLinkedList(values);
+
+            var actual = sut.OddEvenList(node);
+            Assert.Equal(expected, LinkedListHelper.ListFromListNode(actual));
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, 2,1 }, true)]
+        [InlineData(new int[] { 1, 2,   2, 1 }, true)]
+        [InlineData(new int[] { 1, 2 }, false)]
+        [InlineData(new int[] { 1}, true)]
+     //   [InlineData(new int[] {  }, false)]
+        public void Test_IsPalindrome(int[] values, bool expected)
+        {
+            ListNode node = LinkedListHelper.CreateLinkedList(values);
+
+            var actual = sut.IsPalindrome(node);
+            Assert.Equal(expected, actual);
+        }
+        
     }
 }
