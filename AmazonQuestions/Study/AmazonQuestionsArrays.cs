d2c600bcefaf1710c430f417167924007cf1f3c6
Marcus Quigley <marcuquigley@gmail.com>
2020-06-04 18:05:00 -0400

    AZ Q 1 Two Sum

---
+++ AmazonQuestions/Study/AmazonQuestionsArrays.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AmazonQuestions.Study
+{
+    public class AmazonQuestionsArrays
+    {
+
+        //Easy https://leetcode.com/problems/two-sum/
+        public int[] TwoSum(int[] nums, int target)
+        {
+            if (nums == null)
+                throw new ArgumentNullException(nameof(nums));
+            var result = new int[2];
+            var n = nums.Length;
+            Dictionary<int, int> map = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                var current = nums[i];
+                var diff = target - current;
+
+                if (map.ContainsKey(diff))
+                {
+                    result[0] = map[diff];
+                    result[1] = i;
+                    break;
+                }
+                if (!map.ContainsKey(current))
+                    map.Add(current, i);
+
+            }
+            return result;
+        }
+
+        ////Medium https://leetcode.com/problems/longest-substring-without-repeating-characters/
+        //public int LengthOfLongestSubstring(string s)
+        //{
+        //    //pwwkew
+        //    if (s == null)
+        //        throw new ArgumentNullException();
+        //    if (s.Length==0)
+        //        return 0;
+
+        //    var begin = 0;
+        //    var end = 0;
+        //    var n = s.Length;
+        //    var max = 0;
+        //    while (end < n)
+        //    {
+        //        var val = s[end];
+        //        var i = begin;
+        //        while (i < end)
+        //        {
+        //            if (s[i] == val)
+        //            {
+        //                max = Math.Max(max, end - begin);
+        //                begin = i+1;
+        //                break;
+        //            }
+        //            i++;
+        //        }
+        //        end++;
+        //    }
+        //    max = Math.Max(max, end - begin );
+        //    return max;
+        //}
+
+    }
+}
---
+++ Tests/AmazonQuestions.Tests/Study/AmazonArrayTests.cs
@@ -0,0 +1,43 @@
+using AmazonQuestions.Study;
+using System;
+using Xunit;
+
+namespace AmazonQuestions.Tests
+{
+    public class AmazonArrayTests
+    {
+        readonly AmazonQuestionsArrays sut;
+        public AmazonArrayTests()
+        {
+            sut = new AmazonQuestionsArrays();
+        }
+        [Theory]
+        [InlineData(new int[] { 2, 7, 11, 15 }, 9, new int[] { 0,1})]
+        [InlineData(new int[] { 4, 0,2 }, 4, new int[] { 0, 1 })]
+        [InlineData(new int[] { 3,3 }, 6, new int[] { 0, 1 })]
+        [InlineData(new int[] { -3, 4, 3, 90 }, 0, new int[] { 0, 2 })]
+        [InlineData(new int[] { 230, 863, 916, 585, 981, 404, 316, 785, 88, 12, 70, 435, 384, 778, 887, 755, 740, 337, 86, 92, 325, 422, 815, 650, 920, 125, 277, 336, 221, 847, 168, 23, 677, 61, 400, 136, 874, 363, 394, 199, 863, 997, 794, 587, 124, 321, 212, 957, 764, 173, 314, 422, 927, 783, 930, 282, 306, 506, 44, 926, 691, 568, 68, 730, 933, 737, 531, 180, 414, 751, 28, 546, 60, 371, 493, 370, 527, 387, 43, 541, 13, 457, 328, 227, 652, 365, 430, 803, 59, 858, 538, 427, 583, 368, 375, 173, 809, 896, 370, 789 }
+        , 542, new int[] { 28,45 })]
+        public void Test_TwoSum(int[] numbers, int target, int[] expected)
+        {
+            var actual = sut.TwoSum(numbers,target);
+            Assert.Equal(expected, actual);
+
+        }
+        //[Theory]
+        //[InlineData("abcabcbb", 3)]
+        //[InlineData("bbbb", 1)]
+        //[InlineData("pwwkew", 3)]
+        //[InlineData("", 0)]
+        //[InlineData("a", 1)]
+        //[InlineData("dvdf", 3)]
+        //public void Test_LengthOfLongestSubstring(string s, int expected)
+        //{
+        //    var actual = sut.LengthOfLongestSubstring(s);
+        //    Assert.Equal(expected, actual);
+
+        //}
+        
+
+    }
+}
