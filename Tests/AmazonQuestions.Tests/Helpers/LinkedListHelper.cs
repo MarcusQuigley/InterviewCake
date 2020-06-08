91034f3fcbc3af3655043091466b095c60c9a687
Marcus Quigley <marcuquigley@gmail.com>
2020-06-08 17:33:25 -0400

    AZ Q 2 Add Two Numbers

---
+++ Tests/AmazonQuestions.Tests/Helpers/LinkedListHelper.cs
@@ -0,0 +1,41 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AmazonQuestions.Tests.Helpers
+{
+    public static class LinkedListHelper
+    {
+        #region Helpers
+
+        public static ListNode CreateLinkedList(int[] values)
+        {
+            var head = new ListNode(values[0]);
+            var current = head;
+            for (int i = 1; i < values.Length; i++)
+            {
+                //  while(current.next !=null)
+                current.next = new ListNode(values[i]);
+                current = current.next;
+            }
+            return head;
+        }
+
+        public static int[] ListFromListNode(ListNode node)
+        {
+            List<int> vals = new List<int>();
+            if (node != null)
+            {
+                while (node != null)
+                {
+                    vals.Add(node.val);
+                    node = node.next;
+                }
+            }
+            return vals.ToArray();
+        }
+        #endregion
+    }
+}
+ 
--- Trees/LeetCode/LeetEasyTrees.cs
+++ Trees/LeetCode/LeetEasyTrees.cs
@@ -2,7 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
-using DataStructures;
+ 
 
 namespace Trees.LeetCode
 {
