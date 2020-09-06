ed34c8f35e918493f10376a4288af7ca8b3db358
Marcus Quigley <marcusquigley@gmail.com>
2020-09-06 13:24:44 -0400

    Leet 121. Best Time to Buy and Sell Stock

---
+++ Tests/DynamicProgramming.Tests/LeetCode/LeetEasyDPTests.cs
@@ -0,0 +1,28 @@
+﻿using DynamicProgramming.LeetCode;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DynamicProgramming.Tests.LeetCode
+{
+    public class LeetEasyDPTests
+    {
+        LeetEasyDP sut = null;
+        public LeetEasyDPTests()
+        {
+            sut = new LeetEasyDP();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 7, 1, 5, 3, 6, 4 }, 5)]
+        [InlineData(new int[] { 7, 6, 4, 3, 1 }, 0)]
+        [InlineData(new int[] {  1,2 }, 1)]
+        public void Test_BuyAndSellStockDp(int[] prices, int expected)
+        {
+            var actual = sut.MaxProfit(prices);
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
