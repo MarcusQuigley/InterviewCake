c86b987f1681d8cee39ff7113a4285ab7bb69ab9
Marcus Quigley <marcusquigley@gmail.com>
2020-09-08 08:13:50 -0400

    Leet Ex Binary Tree Preorder Traversal + recursion

---
+++ Tests/Trees.Tests/LeetCode/BinaryTreeStudyChapterTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trees.LeetCode;
+using Xunit;
+namespace Trees.Tests.LeetCode
+{
+    public class BinaryTreeStudyChapterTests : BaseTreeTests
+    {
+        BinaryTreeStudyChapter sut;
+
+        public BinaryTreeStudyChapterTests()
+        {
+            sut = new BinaryTreeStudyChapter();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 4, 3, 2 }, new int[] { 1, 4, 2, 3 })]
+        [InlineData(new int[] { 1, -666, 2, -666, -666, 3 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { }, new int[] { })]
+        public void Test_PreorderTraversalRecursive(int[] values, int[] expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+            var actual = sut.PreorderTraversalRecursive(root);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 4, 3, 2 }, new int[] { 1, 4, 2, 3 })]
+        [InlineData(new int[] { 1, -666, 2, -666, -666, 3 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { }, new int[] { })]
+        public void Test_PreorderTraversal(int[] values, int[] expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+            var actual = sut.PreorderTraversal(root);
+            Assert.Equal(expected, actual);
+
+        }
+
+        //[Theory]
+        //[InlineData(new int[] { 1, 4, 3, 2 }, new int[] { 2,4,1,3 })]
+        //[InlineData(new int[] { 1, -666, 2, -666, -666, 3 }, new int[] { 1, 3, 2 })]
+        //[InlineData(new int[] { }, new int[] { })]
+        //public void Test_InorderTraversal(int[] values, int[] expected)
+        //{
+        //    var root = base.CreatTreeNodesNonGeneric(values);
+        //    var actual = sut.InorderTraversal(root);
+        //    Assert.Equal(expected, actual);
+
+        //}
+
+        //[Theory]
+        //[InlineData(new int[] { 1, 4, 3, 2 }, new int[] { 2, 4, 1, 3 })]
+        //[InlineData(new int[] { 1, -666, 2, -666, -666, 3 }, new int[] { 1, 3, 2 })]
+        //[InlineData(new int[] { }, new int[] { })]
+        //public void Test_InorderTraversalRecursion(int[] values, int[] expected)
+        //{
+        //    var root = base.CreatTreeNodesNonGeneric(values);
+        //    var actual = sut.InorderTraversalRecursion(root);
+        //    Assert.Equal(expected, actual);
+
+        //}
+    }
+}
---
+++ Trees/LeetCode/BinaryTreeStudyChapter.cs
@@ -0,0 +1,90 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.LeetCode
+{
+    public class BinaryTreeStudyChapter
+    {
+        //https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/928/
+        List<int> resultsPreorderRecursive = new List<int>();
+        public IList<int> PreorderTraversalRecursive(TreeNode root)
+        {//Root,L,R
+            if (root != null)
+                PreorderTraversalRecursiveWork(root);
+            return resultsPreorderRecursive;
+        }
+
+        void PreorderTraversalRecursiveWork(TreeNode node)
+        {
+            if (node == null)
+                return;
+            resultsPreorderRecursive.Add(node.val);
+            PreorderTraversalRecursiveWork(node.left);
+            PreorderTraversalRecursiveWork(node.right);
+
+        }
+        //https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/928/
+
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            List<int> results = new List<int>();
+            if (root == null)
+                return results; 
+            
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count>0)
+            {
+                TreeNode current = stack.Pop();
+                results.Add(current.val);
+                if (current.right != null) stack.Push(current.right);
+                if (current.left != null) stack.Push(current.left);
+               
+            }
+
+            return results;
+        }
+        ////https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/
+        //public IList<int> InorderTraversal(TreeNode root)
+        //{
+        //    List<int> results = new List<int>();
+        //    if (root == null)
+        //        return results;
+
+        //    TreeNode current = root;
+        //    Stack<TreeNode> stack = new Stack<TreeNode>();
+        //    while(stack.Count>0 || current!=null)
+        //    {
+        //        while(current!= null  )
+        //        {
+                 
+        //            stack.Push(current);
+        //            current = current.left;
+        //        }
+        //        current = stack.Pop();
+        //        results.Add(current.val);
+        //        current = current.right;
+        //    }
+            
+        //    return results;
+
+        //}
+        ////https://leetcode.com/explore/learn/card/data-structure-tree/134/traverse-a-tree/929/
+        //public IList<int> InorderTraversalRecursion(TreeNode root)
+        //{
+        //    List<int> result = new List<int>();
+        //    InorderWork(root, result);
+        //    return result;
+        //}
+        //void InorderWork(TreeNode root, List<int> results)
+        //{
+        //    if (root == null)
+        //        return;
+        //    InorderWork(root.left, results);
+        //    results.Add(root.val);
+        //    InorderWork(root.right, results);
+        //}
+    }
+}
