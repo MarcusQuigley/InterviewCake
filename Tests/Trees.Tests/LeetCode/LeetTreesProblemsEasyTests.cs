c9d5c0685a4f52e2755932757ff0063c77ccdbe9
Marcus Quigley <marcusquigley@gmail.com>
2020-09-10 14:42:55 -0400

    Leet Tree Probs Easy Get Lonely Nodes

---
+++ Tests/Trees.Tests/LeetCode/LeetTreesProblemsEasyTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Trees.LeetCode;
+using Xunit;
+namespace Trees.Tests.LeetCode
+{
+    public class LeetTreesProblemsEasyTests : BaseTreeTests
+    {
+        LeetTreesProblemsEasy sut;
+
+        public LeetTreesProblemsEasyTests()
+        {
+            sut = new LeetTreesProblemsEasy();
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2, 3, -666, 4 }, new int[] { 4 })]
+        [InlineData(new int[] { 7, 1, 4, 6, -666, 5, 3, -666, -666, -666, -666, -666, -666, -666, 2 }, new int[] { 6,2 })]
+        [InlineData(new int[] { 11, 99, 88, 77, -666, -666, 66, 55, -666, -666, -666, -666, -666, -666, 44  }, new int[] { 77, 55,  66, 44   })]
+        [InlineData(new int[] { 11, 99, 88, 77, -666, -666, 66, 55, -666, -666, -666, -666, -666, -666, 44, 33, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666,-666,-666, 22 }, new int[] { 77, 55, 33, 66, 44, 22 })]
+        public void Test_GetLonelyNodes(int[] values , int[] expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+            var actual = sut.GetLonelyNodes(root);
+            Assert.Equal(expected, actual);
+
+        }
+
+        [Theory]
+        [InlineData(new int[] { 1, 2 }, new int[] { 2})]
+        [InlineData(new int[] { 1, 2, 3  }, new int[] {  })]
+        [InlineData(new int[] { 1, 2, 3, -666, 4 }, new int[] { 4 })]
+        [InlineData(new int[] { 7, 1, 4, 6, -666, 5, 3, -666, -666, -666, -666, -666, -666, -666, 2 }, new int[] { 6, 2 })]
+        [InlineData(new int[] { 11, 99, 88, 77, -666, -666, 66 }, new int[] { 77, 66 })]
+        [InlineData(new int[] { 11, 99, 88, 77, -666, -666, 66, 55, -666, -666, -666, -666, -666, -666, 44 }, new int[] { 77, 66, 55,  44 })]
+        [InlineData(new int[] { 11, 99, 88, 77, -666, -666, 66, 55, -666, -666, -666, -666, -666, -666, 44, 33, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666, -666, 22 }, new int[] { 77, 66,55,44,33,22 })]
+        public void Test_GetLonelyNodesIter(int[] values, int[] expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+            var actual = sut.GetLonelyNodesIter(root);
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
---
+++ Trees/LeetCode/LeetTreesProblemsEasy.cs
@@ -0,0 +1,59 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees.LeetCode
+{
+    public class LeetTreesProblemsEasy
+    {
+
+        //https://leetcode.com/problems/find-all-the-lonely-nodes/
+        public IList<int> GetLonelyNodes(TreeNode root)
+        {
+            if (root == null)
+                return new int[] { };
+            IList<int> results = new List<int>();
+            GetLonelyNodesWorker(root, results);
+            return results;
+        }
+
+        void GetLonelyNodesWorker(TreeNode root, IList<int> results)
+        {
+            if (root == null)
+                return;
+
+            if (root.left == null && root.right != null)
+                results.Add(root.right.val);
+            if (root.right == null && root.left != null)
+                results.Add(root.left.val);
+            GetLonelyNodesWorker(root.left, results);
+            GetLonelyNodesWorker(root.right, results);
+
+        }
+
+        public IList<int> GetLonelyNodesIter(TreeNode root)
+        {
+            if (root == null)
+                return new int[] { };
+            IList<int> results = new List<int>();
+            Queue<TreeNode> q = new Queue<TreeNode>();
+            q.Enqueue(root);
+            while (q.Count > 0)
+            {
+                var node = q.Dequeue();
+                if (node.left == null && node.right != null)
+                {
+                    results.Add(node.right.val);
+                }
+                if (node.right == null && node.left != null)
+                {
+                    results.Add(node.left.val);
+                }
+                if (node.left != null) q.Enqueue(node.left);
+                if (node.right != null) q.Enqueue(node.right);
+            }
+            return results;
+        } 
+    }
+}
