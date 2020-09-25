b387c8818e25b7231cef3628b03a6e5d9d79c386
Marcus Quigley <marcusquigley@gmail.com>
2020-09-25 12:43:39 -0400

    Leet Tree Probs v2 Range sum

---
+++ Tests/Trees.Tests/LeetCode/LeetTreesProblemsEasyV2Tests.cs
@@ -0,0 +1,42 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Trees.LeetCode;
+using Xunit;
+namespace Trees.Tests.LeetCode
+{
+    public class LeetTreesProblemsEasyV2Tests : BaseTreeTests
+    {
+        LeetTreesProblemsEasyV2 sut;
+
+        public LeetTreesProblemsEasyV2Tests()
+        {
+            sut = new LeetTreesProblemsEasyV2();
+        }
+ 
+        [Theory]
+        [InlineData(new int[] { 5, 1, 7, -666, -666, -666, 8 }, 3, 5, 5)]
+         [InlineData(new int[] { 10, 5, 15, 3, 7, -666, 18 }, 7, 15, 32)]
+        [InlineData(new int[] { 10, 5, 15, 3, 7, 13, 18, 1, -666, 6 }, 6, 10, 23)]
+        public void Test_RangeSumBST(int[] values, int left, int right, int expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+            var actual = sut.RangeSumBST(root, left, right);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(new int[] { 5, 1, 7, -666, -666, -666, 8 }, 3, 5, 5)]
+         [InlineData(new int[] { 10, 5, 15, 3, 7, -666, 18 }, 7, 15, 32)]
+        [InlineData(new int[] { 10, 5, 15, 3, 7, 13, 18, 1, -666, 6 }, 6, 10, 23)]
+        public void Test_RangeSumBSTIter(int[] values, int left, int right, int expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+            var actual = sut.RangeSumBSTIter(root, left, right);
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
---
+++ Trees/LeetCode/LeetTreesProblemsEasyV2.cs
@@ -0,0 +1,39 @@
+﻿using DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Trees.LeetCode
+{
+    public class LeetTreesProblemsEasyV2
+    {
+        public int RangeSumBST(TreeNode root, int L, int R)
+        {
+            if (root == null)
+                return 0;
+            var sum = (root.val >= L && root.val <= R) ? root.val : 0;
+            var left = (root.val >= L) ? RangeSumBST(root.left, L, R) : 0;
+            var right = (root.val <= R) ? RangeSumBST(root.right, L, R) : 0;
+
+            return left + right + sum;
+        }
+
+        public int RangeSumBSTIter(TreeNode root, int L, int R)
+        {
+            if (root == null) return 0;
+            var sum = 0;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0)
+            {
+                var node = stack.Pop();
+                if (node.val >= L && node.val <= R)
+                    sum += node.val;
+                if (node.val >= L && node.left != null ) stack.Push(node.left);
+                if (node.val <= R && node.right != null) stack.Push(node.right);
+            }
+            return sum;
+        }
+    }
+}
