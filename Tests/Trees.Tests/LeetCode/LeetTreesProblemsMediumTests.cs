db4bac37f0f29a1342c2ffcd1f1bece62f187091
marcus quigley <marcusquigley@gmail.com>
2020-11-24 08:00:18 -0500

    Basic Tree Traversal

--- Tests/Trees.Tests/LeetCode/LeetTreesProblemsMediumTests.cs
+++ Tests/Trees.Tests/LeetCode/LeetTreesProblemsMediumTests.cs
@@ -1,9 +1,4 @@
-﻿using DataStructures;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Trees.LeetCode;
+﻿using Trees.LeetCode;
 using Xunit;
 namespace Trees.Tests.LeetCode
 {
@@ -17,10 +12,10 @@ namespace Trees.Tests.LeetCode
         }
 
         [Theory]
-        [InlineData(new int[] {1,-666,2, -666, -666, -666,3 }, new int[] { 3,2,1})]
-        [InlineData(new int[] { 1,2 }, new int[] {  2, 1 })]
-        [InlineData(new int[] { 4,2,9,3,5,-666,7 }, new int[] { 3,5,2,7,9,4 })]
-        [InlineData(new int[] { 1,2,3,4,5,-666,7, -666, -666,6, -666, -666, -666, -666,8 }, new int[] {4,6,5,2,8,7,3,1})]
+        [InlineData(new int[] { 1, -666, 2, -666, -666, -666, 3 }, new int[] { 3, 2, 1 })]
+        [InlineData(new int[] { 1, 2 }, new int[] { 2, 1 })]
+        [InlineData(new int[] { 4, 2, 9, 3, 5, -666, 7 }, new int[] { 3, 5, 2, 7, 9, 4 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, -666, 7, -666, -666, 6, -666, -666, -666, -666, 8 }, new int[] { 4, 6, 5, 2, 8, 7, 3, 1 })]
         public void Test_PostorderTraversal(int[] values, int[] expected)
         {
             var root = base.CreatTreeNodesNonGeneric(values);
@@ -28,6 +23,35 @@ namespace Trees.Tests.LeetCode
             var actual = sut.PostorderTraversal(root);
             Assert.Equal(expected, actual);
         }
-        
+
+        [Theory]
+        [InlineData(new int[] { 1, -666, 2, -666, -666, -666, 3 }, new int[] { 1, 2, 3 })]
+        [InlineData(new int[] { 1, 2 }, new int[] { 1, 2 })]
+        [InlineData(new int[] { 4, 2, 8, 1, 3, 6 }, new int[] { 4, 2, 1, 3, 8, 6 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, -666, 7, -666, -666, 6, -666, -666, -666, -666, 8 }, new int[] { 1, 2, 4, 5, 6, 3, 7, 8 })]
+        public void Test_PreorderTraversal(int[] values, int[] expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+
+            var actual = sut.PreorderTraversal(root);
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+
+        [InlineData(new int[] { 1, 2 }, new int[] { 2, 1 })]
+        [InlineData(new int[] { 4, 2, 8, 1, 3, 6 }, new int[] { 1, 3, 2, 6, 8, 4 })]
+        [InlineData(new int[] { 1, 2, 3, 4, 5, -666, 7, -666, -666, 6, -666, -666, -666, -666, 8 }, new int[] { 4, 6, 5, 2, 8, 7, 3, 1 })]
+        [InlineData(new int[] { 1, 4, 3, 2 }, new int[] { 2, 4, 3, 1 })]
+        [InlineData(new int[] { 1, -666, 2, -666, -666, 3 }, new int[] { 3, 2, 1 })]
+        [InlineData(new int[] { }, new int[] { })]
+        public void Test_PostOrderTraversalIntuitive(int[] values, int[] expected)
+        {
+            var root = base.CreatTreeNodesNonGeneric(values);
+
+            var actual = sut.PostOrderTraversalIntuitive(root);
+            Assert.Equal(expected, actual);
+        }
+
     }
 }
--- Trees/LeetCode/LeetTreesProblemsMedium.cs
+++ Trees/LeetCode/LeetTreesProblemsMedium.cs
@@ -1,13 +1,10 @@
 ﻿using DataStructures;
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Trees.LeetCode
 {
     public class LeetTreesProblemsMedium
-    { 
+    {
         //https://leetcode.com/problems/binary-tree-postorder-traversal/
         public IList<int> PostorderTraversal(TreeNode root)
         {
@@ -16,15 +13,12 @@ namespace Trees.LeetCode
                 return results;
             Stack<TreeNode> stack = new Stack<TreeNode>();
             TreeNode current = root;
-            while (stack.Count > 0 || current != null)
-            {
-                if (current != null)
-                {
+            while (stack.Count > 0 || current != null) {
+                if (current != null) {
                     stack.Push(current);
                     current = current.left;
                 }
-                else
-                {
+                else {
                     var temp = stack.Peek().right;
                     if (temp == null) //its as leaf node
                     {
@@ -34,13 +28,52 @@ namespace Trees.LeetCode
                         {
                             temp = stack.Pop();
                             results.Add(temp.val);
-                         }
+                        }
                     }
                     else
                         current = temp;
                 }
-             }
+            }
             return results;
+        }
+
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> list = new List<int>();
+            if (root != null) {
+                Stack<TreeNode> stack = new Stack<TreeNode>();
+                stack.Push(root);
+                while (stack.Count > 0) {
+                    var current = stack.Pop();
+                    list.Add(current.val);
+                    if (current.right != null) stack.Push(current.right);
+                    if (current.left != null) stack.Push(current.left);
+                }
+            }
+            return list;
+        }
+
+        public IList<int> PostOrderTraversalIntuitive(TreeNode root)
+        {
+            IList<int> list = new List<int>();
+            if (root == null)
+                return list;
+
+            HashSet<TreeNode> set = new HashSet<TreeNode>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while (stack.Count > 0) {
+                var current = stack.Pop();
+                if (set.Contains(current))
+                    list.Add(current.val);
+                else {
+                    set.Add(current);
+                    stack.Push(current);
+                    if (current.right != null) stack.Push(current.right);
+                    if (current.left != null) stack.Push(current.left);
+                }
+            }
+            return list;
         }
     }
 }
